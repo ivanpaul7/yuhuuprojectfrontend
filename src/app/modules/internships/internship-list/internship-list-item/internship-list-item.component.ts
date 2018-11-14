03ad934f922c80f967500f300fc5d14181702fcf
Callbaza <alex.sebastian.calbaza@gmail.com>
2018-11-14 19:16:26 +0200

    Creare componente internship

---
+++ src/app/modules/internships/internship-list/internship-list-item/internship-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-internship-list-item',
+  templateUrl: './internship-list-item.component.html',
+  styleUrls: ['./internship-list-item.component.scss']
+})
+export class InternshipListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/internships/internships.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { InternshipsRoutingModule } from './internships-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    InternshipsRoutingModule
+  ],
+  declarations: []
+})
+export class InternshipsModule { }
