93d22319b2ddb7bddfba38e227543ec6e598a68a
Callbaza <alex.sebastian.calbaza@gmail.com>
2018-12-09 00:27:38 +0200

    New type of internships list

--- src/app/modules/internships/internships.module.ts
+++ src/app/modules/internships/internships.module.ts
@@ -4,11 +4,13 @@ import { InternshipsRoutingModule } from './internships-routing.module';
 import { InternshipListComponent } from './components/internship-list/internship-list.component';
 import { InternshipListItemComponent } from './components/internship-list-item/internship-list-item.component';
 import { InternshipsService } from './services/internships.service';
+import { MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    InternshipsRoutingModule
+    InternshipsRoutingModule,
+    MatCardModule
   ],
   declarations: [InternshipListComponent, InternshipListItemComponent],
   exports: [InternshipListComponent, InternshipListItemComponent],
--- src/app/modules/internships/services/internships.model.ts
+++ src/app/modules/internships/services/internships.model.ts
@@ -3,11 +3,25 @@ export class Internship {
   public description: string;
   public deadline: string;
   public employmentType: string;
+  public active: boolean;
+  public startDate: string;
+  public endDate: string;
+  public freeSpots: number;
+  public status: string;
+  public companyLogo: string;
 
-  constructor(title: string, description: string, deadline: string, employmentType: string) {
+  constructor(title: string, description: string, deadline: string,
+    employmentType: string, companyLogo: string, startDate: string,
+     endDate: string, freeSpots?: number, status?: string, active?: boolean,) {
     this.title = title;
     this.deadline = deadline;
     this.description = description;
     this.employmentType = employmentType;
+    this.active = active;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.freeSpots = freeSpots;
+    this.status = status;
+    this.companyLogo = companyLogo;
   }
 }
