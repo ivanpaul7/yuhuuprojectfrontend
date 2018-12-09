72d6a95b2b241f205f7165e56193ed66522bea9a
Callbaza <alex.sebastian.calbaza@gmail.com>
2018-12-09 23:40:34 +0200

    Sters comentarii

--- src/app/modules/internships/services/internships.service.ts
+++ src/app/modules/internships/services/internships.service.ts
@@ -31,6 +31,39 @@ export class InternshipsService {
             '2012/10/11',
             '2013/10/11'
         ),
+        new Internship(
+            'Un internship',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
+            '12/12/2022',
+            'JAVA',
+            'https://ardgowanhospice.org.uk/wp-content/uploads/2018/09/1920x1080-brands-amazon-logo.jpg',
+            '2012/10/11',
+            '2013/10/11'
+        ),  new Internship(
+            'Un internship',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
+            '12/12/2022',
+            'JAVA',
+            'https://ardgowanhospice.org.uk/wp-content/uploads/2018/09/1920x1080-brands-amazon-logo.jpg',
+            '2012/10/11',
+            '2013/10/11'
+        ),  new Internship(
+            'Un internship',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
+            '12/12/2022',
+            'JAVA',
+            'https://ardgowanhospice.org.uk/wp-content/uploads/2018/09/1920x1080-brands-amazon-logo.jpg',
+            '2012/10/11',
+            '2013/10/11'
+        ),  new Internship(
+            'Un internship',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
+            '12/12/2022',
+            'JAVA',
+            'https://ardgowanhospice.org.uk/wp-content/uploads/2018/09/1920x1080-brands-amazon-logo.jpg',
+            '2012/10/11',
+            '2013/10/11'
+        ),
     ];
 
     constructor() {}
