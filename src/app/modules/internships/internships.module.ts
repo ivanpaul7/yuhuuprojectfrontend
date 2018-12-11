e83e0dd8b424f565c318f199745d3a670c2439f9
Alex-Sebastian Calbaza <alex.sebastian.calbaza@gmail.com>
2018-12-11 22:55:17 +0000

    New type of internships list

--- src/app/modules/internships/internships.module.ts
+++ src/app/modules/internships/internships.module.ts
@@ -4,13 +4,18 @@ import { InternshipsRoutingModule } from './internships-routing.module';
 import { InternshipListComponent } from './components/internship-list/internship-list.component';
 import { InternshipListItemComponent } from './components/internship-list-item/internship-list-item.component';
 import { InternshipsService } from './services/internships.service';
+import { MatCardModule, MatCheckboxModule, MatFormFieldModule } from '@angular/material';
+import { FiltersComponent } from './components/filters/filters.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    InternshipsRoutingModule
+    InternshipsRoutingModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule
   ],
-  declarations: [InternshipListComponent, InternshipListItemComponent],
+  declarations: [InternshipListComponent, InternshipListItemComponent, FiltersComponent],
   exports: [InternshipListComponent, InternshipListItemComponent],
   providers: [InternshipsService]
 })
--- src/app/modules/internships/services/internships.service.ts
+++ src/app/modules/internships/services/internships.service.ts
@@ -1,26 +1,68 @@
 import { Injectable } from '@angular/core';
-import { Internship } from './internships.model';
+import { Internship } from '../../../shared/model/internships.model';
 
 @Injectable()
 export class InternshipsService {
     private internships: Internship[] = [
         new Internship(
             'La fortech',
-            'stam 8 ore',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
             'sambata viitoare',
-            'multa info'
+            'multa info',
+            'https://image.shutterstock.com/image-photo/valencia-spain-march-05-2017-260nw-593204357.jpg',
+            '2012/10/11',
+            '2013/10/11'
         ),
         new Internship(
             'La arobs',
-            'stam cat stam',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
             'vineri',
-            'prezentare dupa prezentare'
+            'prezentare dupa prezentare',
+            'https://cdn4.iconfinder.com/data/icons/new-google-logo-2015/400/new-google-favicon-512.png',
+            '2012/10/11',
+            '2013/10/11'
         ),
         new Internship(
             'Un internship',
-            'Acesta este un internship moca.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
             '12/12/2022',
-            'JAVA'
+            'JAVA',
+            'https://ardgowanhospice.org.uk/wp-content/uploads/2018/09/1920x1080-brands-amazon-logo.jpg',
+            '2012/10/11',
+            '2013/10/11'
+        ),
+        new Internship(
+            'Un internship',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
+            '12/12/2022',
+            'JAVA',
+            'https://ardgowanhospice.org.uk/wp-content/uploads/2018/09/1920x1080-brands-amazon-logo.jpg',
+            '2012/10/11',
+            '2013/10/11'
+        ),  new Internship(
+            'Un internship',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
+            '12/12/2022',
+            'JAVA',
+            'https://ardgowanhospice.org.uk/wp-content/uploads/2018/09/1920x1080-brands-amazon-logo.jpg',
+            '2012/10/11',
+            '2013/10/11'
+        ),  new Internship(
+            'Un internship',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
+            '12/12/2022',
+            'JAVA',
+            'https://ardgowanhospice.org.uk/wp-content/uploads/2018/09/1920x1080-brands-amazon-logo.jpg',
+            '2012/10/11',
+            '2013/10/11'
+        ),  new Internship(
+            'Un internship',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
+            '12/12/2022',
+            'JAVA',
+            'https://ardgowanhospice.org.uk/wp-content/uploads/2018/09/1920x1080-brands-amazon-logo.jpg',
+            '2012/10/11',
+            '2013/10/11'
         ),
     ];
 
