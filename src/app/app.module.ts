de3bb1cca6aa3354f4d14bac92952c038540a530
DESKTOP-AAR86TH\raree <rareessbeltechi@gmail.com>
2018-12-19 13:05:37 +0200

    for merge request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule, Routes} from '@angular/router';
-import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {AppComponent} from './app.component';
 import {DashboardPageComponent} from './modules/dashboard/pages/dashboard-page/dashboard-page.component';
 import {InternshipsModule} from './modules/internships/internships.module';
@@ -15,8 +14,9 @@ import {LoginModule} from './modules/login/login.module';
 import {LoginPageComponent} from './modules/login/pages/login-page/login-page.component';
 import {StudentProfilePageComponent} from './modules/profile/pages/student-profile-page/student-profile-page.component';
 import {ProfileModule} from './modules/profile/profile.module';
-import {InMemoryDataService} from './modules/profile/services/in-memory-data.service';
 import {DashboardModule} from './modules/dashboard/dashboard.module';
+import {RegisterPageComponent} from './modules/login/pages/register-page/register-page.component';
+import {AlertModule} from './modules/alert/alert.module';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -24,10 +24,11 @@ const appRoutes: Routes = [
   {path: 'login', component: LoginPageComponent, pathMatch: 'full'},
   {
     path: 'dashboard',
-    component:DashboardPageComponent,
+    component: DashboardPageComponent,
     loadChildren: './modules/dashboard/dashboard.module#DashboardModule'
   },
-  {path: 'profile/:id', component: StudentProfilePageComponent, pathMatch: 'full'}
+  {path: 'profile/:id', component: StudentProfilePageComponent, pathMatch: 'full'},
+  {path: 'register', component: RegisterPageComponent, pathMatch: 'full'},
 ];
 
 @NgModule({
