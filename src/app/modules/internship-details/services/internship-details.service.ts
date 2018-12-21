86af7868d7ec078b2c99c6797fc0f549e82b62ce
abulmez <bulmezalexandru97@gmail.com>
2018-12-21 15:42:29 +0000

    Update internship-details.service.ts

--- src/app/modules/internship-details/services/internship-details.service.ts
+++ src/app/modules/internship-details/services/internship-details.service.ts
@@ -12,7 +12,6 @@ export class InternshipDetailsService {
 
 export class MockInternshipDetailsService {
   public getInternship(internshipID:string) : Internship{
-    //return '{"title": "Title Internship", "description": "descriere", "skills": "skills", "attributes":  "attributes", "requirements":"requirements", "tags": ["tag1","tag2"], "type": "full-time", "locuri": 20, "status": "activ", "logo": "https://ontarioarc.org/wp-content/uploads/2015/05/pepsi-logo-vector.jpg", "deadline": 1544313600}'
     return new Internship(
       'Un internship',
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Imperdiet sed euismod nisi porta. Congue quisque egestas diam in arcu cursus euismod. In nibh mauris cursus mattis molestie a. ',
