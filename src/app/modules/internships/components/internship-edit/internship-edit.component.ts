9292df2d4dd8e9d8174fedb6d45b894b4d3f6a33
Chis George <chisgeorge97@gmail.com>
2019-01-15 23:04:14 +0200

    no message

--- src/app/modules/internships/components/internship-edit/internship-edit.component.ts
+++ src/app/modules/internships/components/internship-edit/internship-edit.component.ts
@@ -3,6 +3,7 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import { AbstractInternshipDetailsService } from '../../services/internship-details.service';
 import {DatePipe} from '@angular/common';
 import { Internship } from 'src/app/shared/model/Internship';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-internship-edit',
@@ -13,6 +14,18 @@ export class InternshipEditComponent implements OnInit {
 
   @Output() editSubmitEventEmitter = new EventEmitter();
   internship: Internship;
+
+  selectFormControl = new FormControl('', Validators.required);
+  selectFormControlStatus = new FormControl('', Validators.required);
+  optionsEmploymentType: Object[] = [
+    {name: 'Full Time', value: Internship.EmploymentTypeEnum.FULLTIME },
+    {name: 'Part Time', value: Internship.EmploymentTypeEnum.PARTTIME },
+  ];
+  optionsStatus: Object[] = [
+    {name: 'Active', value: Internship.StatusEnum.ACTIVE },
+    {name: 'Ended', value: Internship.StatusEnum.ENDED },
+    {name: 'Coming', value: Internship.StatusEnum.COMING }
+  ];
 
   constructor(
     public datepipe: DatePipe,
