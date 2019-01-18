a28cb2598be605bb7892b74e35e8143ef773b51f
ivanpaul7 <ivanpaul7@yahoo.com>
2019-01-18 17:58:59 +0200

    Add internship

--- src/app/modules/internships/components/internship-list/internship-list.component.ts
+++ src/app/modules/internships/components/internship-list/internship-list.component.ts
@@ -18,65 +18,64 @@ export class InternshipListComponent implements OnInit {
   isHisProfile = true;
 
   constructor(private internshipsService: AbstractInternshipsService, public dialog: MatDialog) {
-
+    this.internshipsService.getInternships().subscribe(
+      (data: Internship[]) => {
+        this.internships = data;
+        for (const internship of data) {
+          this.internshipsService.getInternshipCompany(internship.id).subscribe(
+            (company: Company) => this.companiesJoin.push({
+              idInternship: internship.id,
+              idCompany: company.id
+            }),
+            error => console.log(error)
+          );
+          this.internshipsService.getInternshipSkills(internship.id).subscribe(
+            (skill: Skill[]) => this.skillsJoin.push({
+              idInternship: internship.id,
+              skills: skill
+            }),
+            error => console.log(error)
+          );
+        }
+      },
+      error => console.log(error)
+    );
   }
 
   ngOnInit() {
-    // this.internshipsService.getInternships().subscribe(
-    //   (data: Internship[]) => {
-    //     this.internships = data;
-    //     for (const internship of data) {
-    //       this.internshipsService.getInternshipCompany(internship.id).subscribe(
-    //         (company: Company) => this.companiesJoin.push({
-    //           idInternship: internship.id,
-    //           idCompany: company.id
-    //         }),
-    //         error => console.log(error)
-    //       );
-    //       this.internshipsService.getInternshipSkills(internship.id).subscribe(
-    //         (skill: Skill[]) => this.skillsJoin.push({
-    //           idInternship: internship.id,
-    //           skills: skill
-    //         }),
-    //         error => console.log(error)
-    //       );
-    //     }
-    //   },
-    //   error => console.log(error)
-    // );
-    // this.internshipsService.companySubject.subscribe(
-    //   (data: Company[]) => this.filterByCompanies(data),
-    //   error => console.log(error)
-    // );
-    // this.internshipsService.skillSubject.subscribe(
-    //   (data: Skill[]) => this.filterBySkills(data),
-    //   error => console.log(error)
-    // );
+    this.internshipsService.companySubject.subscribe(
+      (data: Company[]) => this.filterByCompanies(data),
+      error => console.log(error)
+    );
+    this.internshipsService.skillSubject.subscribe(
+      (data: Skill[]) => this.filterBySkills(data),
+      error => console.log(error)
+    );
   }
 
   filterByCompanies(companyFilters: Company[]) {
-    // this.internshipsService.getInternships().subscribe(
-    //   (data: Internship[]) => {
-    //     this.internships = data;
-    //   });
-    // if (companyFilters.length !== 0) {
-    //   this.internships = this.internships.filter((internship) => {
-    //     const company = this.companiesJoin.find((join) => join.idInternship === internship.id);
-    //     return companyFilters.map((comp) => comp.id).indexOf(company.idCompany) > -1;
-    //   });
-    // }
+    this.internshipsService.getInternships().subscribe(
+      (data: Internship[]) => {
+        this.internships = data;
+      });
+    if (companyFilters.length !== 0) {
+      this.internships = this.internships.filter((internship) => {
+        const company = this.companiesJoin.find((join) => join.idInternship === internship.id);
+        return companyFilters.map((comp) => comp.id).indexOf(company.idCompany) > -1;
+      });
+    }
   }
 
 
   filterBySkills(skillFilters: Skill[]) {
-    // if (skillFilters.length !== 0) {
-    //   this.internships = this.internships.filter((internship) => {
-    //     const skillsFiltered = this.skillsJoin.find((join) => join.idInternship === internship.id);
-    //     return skillsFiltered.skills.filter(
-    //       (skill) => skillFilters.map((sk) => sk.id).indexOf(skill.id) > -1)
-    //       .length === this.internshipsService.skillFilters.length;
-    //   });
-    // }
+    if (skillFilters.length !== 0) {
+      this.internships = this.internships.filter((internship) => {
+        const skillsFiltered = this.skillsJoin.find((join) => join.idInternship === internship.id);
+        return skillsFiltered.skills.filter(
+          (skill) => skillFilters.map((sk) => sk.id).indexOf(skill.id) > -1)
+          .length === this.internshipsService.skillFilters.length;
+      });
+    }
   }
 
   openAddInternshipDialog() {
